9424489fbaa833ffee5b6f17a35f14b30189cefd
DoDuongDuy <duyhoten080502@gmail.com>
2021-03-20 21:20:08 +0700

    fetch

--- my-router/src/views/jobs/JobDetails.vue
+++ my-router/src/views/jobs/JobDetails.vue
@@ -1,16 +1,34 @@
 <template>
   <h1>Job details</h1>
-  <p>{{id_detail}}</p>
+  <div v-if="job">
+    <h2> {{job.title}} </h2>
+    <p> {{job.id}} </p>
+    <p> {{job.details}} </p>
+  </div>
+  <div v-else="job">
+    Loading job.....
+  </div>
 </template>
 
 <script>
 export default {
-  props:["id_detail"]
-//  data(){
-//    return{
-//      ID : this.$route.params.id,
-//    }
-//  }
+  props: ["id_detail"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  //  data(){
+  //    return{
+  //      ID : this.$route.params.id,
+  //    }
+  //  }
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id_detail)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((e) => console.log(e.message));
+  },
 };
 </script>
 
--- my-router/src/views/jobs/Jobs.vue
+++ my-router/src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{name: 'JobDetails',params:{id_detail : job.id}}">
-      <h2> {{job.title}} </h2>
-    </router-link>
+  <div v-if="jobs">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id_detail: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else="jobs">
+    Loading jobs .....
   </div>
 </template>
 
@@ -11,31 +16,32 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "my ux designer1", id: 1, details: "lorem1" },
-        { title: "my ux designer2", id: 2, details: "lorem2" },
-        { title: "my ux designer3", id: 3, details: "lorem3" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((e) => console.log(e.message));
   },
 };
 </script>
 
 <style>
-    .job h2{
-        background: #f4f4f4;
-        padding: 20px;
-        border-radius: 10px;
-        margin: 10px auto;
-        max-width: 600px;
-        color: #444;
-        cursor: pointer;
-    }
-    .job h2:hover{
-        background: #ddd;
-    }
-    .job a{
-        text-decoration: none;
-    }
-    
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  color: #444;
+  cursor: pointer;
+}
+.job h2:hover {
+  background: #ddd;
+}
+.job a {
+  text-decoration: none;
+}
 </style>
